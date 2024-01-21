5a8860e955c6cb71bdd124ad060fc0fec095f109
Abhishek Kumar Pathak <officialabhishekpathak@gmail.com>
2024-01-21 10:48:18 +0530

    Project Setup

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express, {Express} from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT || 3000
+
+app.use(cors({ origin: process.env.FRONTEND_URL}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+mongoose.connect(process.env.MONGODB_URL as string)
+mongoose.connection.on("error", (err)=> {
+    console.log(err)
+})
+
+app.listen(port, () => {
+    console.log("server is running on port: " + port)
+})
+
+process.on("SIGINT", ()=>{
+    mongoose.connection.close();
+})
